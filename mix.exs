ed13c99491461fef3868f2c06d2c7f6e18a5e106
Dallin Osmun <dosmun@gmail.com>
2020-10-09 15:34:14 -0600

    (0.1.1)

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Goblet.MixProject do
   def project do
     [
       app: :goblet,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
